8d00a39c1015c2a51f84a3cc448b03ee6b98b939
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2018-03-16 09:18:29 +0530

    added echo service

--- Chat_Server/server.py
+++ Chat_Server/server.py
@@ -1,23 +1,84 @@
 import socket
 import time
 
-#create socket object
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#host and port
-host = socket.gethostname()
-port = 9999
+class Server():
+    '''
+    Server Class to create a server and listen to client
+    '''
+    def __init__(self):
+        '''
+        initializing the socket, host and port
+        '''
+        self.server_sock = socket.socket(socket.AF_INET,
+                            socket.SOCK_STREAM)
+        self.host = socket.gethostname()
+        self.time_port = 9999
+        self.echo_port = 11223
 
-#bind...connect()
-serversocket.bind((host, port))
-#listen()...
-serversocket.listen(5)
+    def set_time_connection(self):
+        '''
+        method to set up a connection
+        '''
+        try:
+            self.server_sock.bind((self.host, self.time_port))
+            self.server_sock.listen(10)
+        except ConnectionError:
+            print('Cannot create a connection')
 
-while True:
-    clientsocket, client_addr = serversocket.accept()
+    def set_echo_connection(self):
+        '''
+        method to set up a connection
+        '''
+        try:
+            self.server_sock.bind((self.host, self.echo_port))
+            self.server_sock.listen(10)
+        except ConnectionError:
+            print('Cannot create a connection')
 
-    print('Got a connection from {client}'.format(client = client_addr))
-    currentTime = time.ctime(time.time()) + '\r\n'
-    clientsocket.send(currentTime.encode('ascii'))
-    clientsocket.close()
+    def time_services(self):
+        '''
+        method to serve current time
+        '''
+        while True:
+            client_sock, client_addr = self.server_sock.accept()
+            print('Client {client} connected to time service:'.
+                format(client=client_addr))
+
+            currentTime = time.ctime(time.time()) + '\r\n'
+
+            try:
+                client_sock.send(currentTime.encode('ascii'))
+                client_sock.close()
+            except ConnectionError:
+                print('Client not reachable')
+
+    def echo_services(self):
+        '''
+        method to serve echo service
+        '''
+        while True:
+            client_sock, client_addr = self.server_sock.accept()
+            print('Client {client} connected to echo service:'.
+                format(client=client_addr))
+
+            msg = client_sock.recv(1024)
+            if not msg:
+                break
+            try:
+                #client_sock.sendall(msg.encode('ascii'))
+                client_sock.sendall(msg)
+            except ConnectionError:
+                print('Client not reachable')
+            finally:
+                client_sock.close()
+
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.set_echo_connection()
+    server.echo_services()
+    # server.set_time_connection()
+    # server.time_services()
 
